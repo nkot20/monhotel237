d0921d8ce492e14571cf75985e2097c708aa030d
etiennenkot1@gmail.com <etiennenkot1@gmail.com>
2022-05-27 17:05:36 +0200

    frontend et backend

---
+++ back-end/tpdevweb/src/main/java/org/isj/ing3/tp/webapp/monhotel237/tpdevweb/TpdevwebApplication.java
@@ -0,0 +1,13 @@
+package org.isj.ing3.tp.webapp.monhotel237.tpdevweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpdevwebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TpdevwebApplication.class, args);
+	}
+
+}
---
+++ back-end/tpdevweb/src/test/java/org/isj/ing3/tp/webapp/monhotel237/tpdevweb/TpdevwebApplicationTests.java
@@ -0,0 +1,13 @@
+package org.isj.ing3.tp.webapp.monhotel237.tpdevweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TpdevwebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
